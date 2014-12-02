a6d8695dce0f9df5d38c860ed5c471f7d05d2393
Alexander Bendl <alexander.bendl.student@htl-hallein.at>
2014-12-02 14:38:27 +0100

    JsonReader
    JsonReader & JSON Lib

---
+++ Programmierung/OpenDataUtilities/src/digitalsalzburg/OpenDataUtilities.java
@@ -0,0 +1,25 @@
+package digitalsalzburg;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class OpenDataUtilities {
+
+	public static void main(String[] args) {
+
+	}
+
+	public static String[] getDataGigs(String... organisations) {
+		return null;
+	}
+
+	public static String[] getOrganisations() {
+		return null;
+	}
+
+	public static long getTimestampFromDate(String dateformat, String source) throws ParseException {
+		SimpleDateFormat sdf = new SimpleDateFormat(dateformat);
+		Date date = sdf.parse(source);
+		return date.getTime();
+	}
+}
