402dbb146f0ef143083766b89fe14b02843503ca
Ferdinand Brunauer <ferdinand.brunauer.student@htl-hallein.at>
2015-01-05 12:28:46 +0100

    AndroidStudio Push Pfusch

---
+++ Programmierung/ClientDatenbrille/app/src/main/java/htlhallein/at/clientdatenbrille/MainActivity.java
@@ -0,0 +1,49 @@
+package htlhallein.at.clientdatenbrille;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.net.wifi.WifiManager;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.webkit.WebView;
+import android.widget.Toast;
+
+
+public class MainActivity extends Activity {
+
+    private NetworkManager networkManager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        this.networkManager = new NetworkManager(this, (WebView) findViewById(R.id.webView));
+        this.networkManager.start();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch(item.getItemId()) {
+            case R.id.action_settings: {
+                Intent intent = new Intent(this, SettingsActivity.class);
+                this.startActivity(intent);
+                Toast.makeText(this, "TEST", Toast.LENGTH_LONG).show();
+                return true;
+            }
+            default: {
+                return super.onOptionsItemSelected(item);
+            }
+        }
+    }
+}
